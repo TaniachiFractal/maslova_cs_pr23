e8b47527b2c34d3f84953735ced63fb3cae8ab21
TaniachiFractal <tnmslv@yandex.ru>
2024-03-19 16:41:50 +0300

    Add project files.

---
+++ maslova_cs_pr23/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace maslova_cs_pr23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btWord
+            // 
+            this.btWord.Location = new System.Drawing.Point(13, 12);
+            this.btWord.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btWord.Name = "btWord";
+            this.btWord.Size = new System.Drawing.Size(304, 114);
+            this.btWord.TabIndex = 0;
+            this.btWord.Text = "В Ворд";
+            this.btWord.UseVisualStyleBackColor = true;
+            this.btWord.Click += new System.EventHandler(this.btWord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 134);
+            this.Controls.Add(this.btWord);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btWord;
+    }
+}
+
---
+++ maslova_cs_pr23/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace maslova_cs_pr23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] names = { "Alex", "Bonnie", "Clara", "Drake", "Egor" };
+        int[] numbers = { 144, 3232, 335, 4224, 5453 };
+
+        private void btWord_Click(object sender, EventArgs e)
+        {
+            var WordApp = new Word.Application();
+            Word.Document document = WordApp.Documents.Add();
+
+            Word.Paragraph userParagraph = document.Paragraphs.Add();
+            Word.Range userRange = userParagraph.Range;
+            userRange.Text = "Справочник телефонов";
+            userRange.InsertParagraphAfter();
+
+            Word.Paragraph tableParagraph = document.Paragraphs.Add();
+            Word.Range tableRange = tableParagraph.Range;
+            Word.Table numbersTable = document.Tables.Add(tableRange, names.Length, 2);
+            numbersTable.Borders.InsideLineStyle = Word.WdLineStyle.wdLineStyleDoubleWavy;
+            numbersTable.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleDashDotDot;
+
+            Word.Range cellRange;
+            cellRange = numbersTable.Cell(1, 1).Range;
+            cellRange.Text = "ФИО";
+            cellRange = numbersTable.Cell(1, 2).Range;
+            cellRange.Text = "Телефон";
+
+            numbersTable.Rows[1].Range.Bold = 1;
+            numbersTable.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphThaiJustify;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                cellRange = numbersTable.Cell(i + 2, 1).Range;
+                cellRange.Text = names[i];
+                cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                cellRange = numbersTable.Cell(i+2,2).Range;
+                cellRange.Text = numbers[i].ToString();
+                cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+            }
+
+            WordApp.Visible = true;
+            document.SaveAs2(Directory.GetCurrentDirectory() + @"\test.docx");
+        }
+    }
+}
